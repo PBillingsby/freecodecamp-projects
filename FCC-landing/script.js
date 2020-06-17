79797aa8d23ec26de2d884b4c6ee7ff280df6bc5
Phil Billingsby <philbillingsby@gmail.com>
2020-06-16 22:01:55 -0400

    Add javascript for video resize

---
+++ FCC-landing/script.js
@@ -0,0 +1,7 @@
+document.getElementById("video").addEventListener("mouseover", function(e) {
+  e.preventDefault()
+  if (e.target.width === "375") {
+    e.target.width = "750";
+    e.target.height = "415"
+  }
+})
