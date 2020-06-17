de21d6ef396248b5371a476e235c52f5ff3e61d2
Phil Billingsby <philbillingsby@gmail.com>
2020-06-16 21:50:29 -0400

    Add content

---
+++ portfolio/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById('navlink').addEventListener("click", function(e) {
+    console.log(e.target.innerText)
+    displayPage(e);
+  })
+})
+
+function displayPage(event) {
+  let work = document.getElementById("#work");
+  let contact = document.getElementById("#contact");
+  if (event.target.innerText === "work") {
+    work.style.visibility = "";
+    contact.style.visibility = "hidden";
+  }
+  else if (event.target.innerText === "contact") {
+    console.log("contact")
+  }
+  else {
+    work.style.visibility = "hidden";
+    contact.style.visibility = "hidden"
+  }
+}
+
+// ADD TIMEOUTS TO PROJECT ITEMS TO LOAD AT SEPARATE TIMES
