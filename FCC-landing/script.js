6c41ef6d2ed8044c023669e38f90338b01ce1cb8
Phil Billingsby <philbillingsby@gmail.com>
2020-06-17 13:29:51 -0400

    Fix media query

--- FCC-landing/script.js
+++ FCC-landing/script.js
@@ -1,7 +1,7 @@
-document.getElementById("video").addEventListener("mouseover", function(e) {
-  e.preventDefault()
-  if (e.target.width === "375") {
-    e.target.width = "750";
-    e.target.height = "415"
-  }
-})
+// document.getElementById("video").addEventListener("mouseover", function(e) {
+//   e.preventDefault()
+//   if (e.target.width === "375") {
+//     e.target.width = "750";
+//     e.target.height = "415"
+//   }
+// })
