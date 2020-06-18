09a3f78dcc71022fed82b5b6adfc852b0b498cd8
Phil Billingsby <philbillingsby@gmail.com>
2020-06-18 14:53:52 -0400

    Fix header background

--- portfolio/script.js
+++ portfolio/script.js
@@ -6,18 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function displayPage(event) {
-  let work = document.getElementById("#work");
-  let contact = document.getElementById("#contact");
-  if (event.target.innerText === "work") {
-    work.style.visibility = "";
-  }
-  else if (event.target.innerText === "contact") {
-    console.log("contact")
-  }
-  else {
-    work.style.visibility = "hidden";
-    contact.style.visibility = "hidden"
-  }
+  // let work = document.getElementById("#work");
+  // let contact = document.getElementById("#contact");
+  // if (event.target.innerText === "work") {
+  //   work.style.visibility = "";
+  // }
+  // else if (event.target.innerText === "contact") {
+  //   console.log("contact")
+  // }
+  // else {
+  //   work.style.visibility = "hidden";
+  //   contact.style.visibility = "hidden"
+  // }
 }
 
 // ADD TIMEOUTS TO PROJECT ITEMS TO LOAD AT SEPARATE TIMES
